51caf4bf7c0c0814597724166a56727964e325bc
eugene254-ship-it <eugene.ochako@strathmore.edu>
2023-04-01 01:24:35 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:grocery_app/screens/register_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(
+        const Duration(
+          seconds: 3,
+        ), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const RegisterScreen()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Hero(tag: 'logo', child: Image.asset('images/logo.png')),
+      ),
+    );
+  }
+}
